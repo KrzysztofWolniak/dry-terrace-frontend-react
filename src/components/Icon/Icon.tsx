096ecd17e50fc80707c7b6301e3038a87ebadb9c
KrzysztofWolniak <261288@student.pwr.edu.pl>
2022-09-02 22:59:53 +0200

    Upgraded icon component, added icontype,context

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,25 +1,33 @@
 
+import { type } from '@testing-library/user-event/dist/type';
 import React, { Component } from 'react'
+import { IconContext } from "react-icons";
+
 interface IconProps{
-  size: number;
+  icon:IconType;
+  size: string;
   color: string;
-  width?: number;
-  height?: number;
+  className?:string;
   orientation?: string;
   text?: string;
   onHover?: boolean;
 }
 
+type IconType = (props:IconProps)=>JSX.Element
+
 export default class Icon extends Component<IconProps,any> {
     
     render() {
-    console.log(this.props.width)
+    console.log(typeof this.props.icon)
        
       return (
-        <div>
-          size: {this.props.size} color: {this.props.color}  {this.props.width!==undefined? <>width: {this.props.width}</> : null }
-          
+        <IconContext.Provider value={{size:this.props.size, color:this.props.color, className:this.props.className  }}>
+
+        <div className='test3'>
+          {React.createElement(this.props.icon )}
+          <a className='test'>{this.props.text}</a>
         </div>
+        </IconContext.Provider>
        
       )
   }
