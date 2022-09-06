f90752b9fa75cb5e39a1edddee458f7c7aabfdc5
KrzysztofWolniak <261288@student.pwr.edu.pl>
2022-09-06 23:51:23 +0200

    Added few more icons to test new functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import React from "react";
 import "./App.css";
 import Icon from "./components/Icon/Icon";
-import {  DiAndroid } from "react-icons/di";
-import {IconFunctionComp} from"./components/Icon/IconFunctionComp"
+import { DiAndroid } from "react-icons/di";
+import { IconFunctionComp } from "./components/Icon/IconFunctionComp";
 
 function App() {
   return (
     <div className="App">
-      
-      <IconFunctionComp size="48" color="red" text="Function " icon={DiAndroid}/>
+      <IconFunctionComp
+        size="48"
+        color="red"
+        text="Function "
+        icon={DiAndroid}
+      />
 
-      <IconFunctionComp size="48" color="red" text="Function Component" icon={DiAndroid}/>
-
-
+      <IconFunctionComp
+        size="48"
+        color="red"
+        text="Function Component"
+        icon={DiAndroid}
+      />
+      <IconFunctionComp
+        size="48"
+        color="red"
+        text="Another Comp"
+        icon={DiAndroid}
+      />
+      <IconFunctionComp
+        size="48"
+        color="red"
+        text="Elo my friend"
+        icon={DiAndroid}
+      />
+      <IconFunctionComp
+        size="48"
+        color="red"
+        text="123123123123123123123"
+        icon={DiAndroid}
+      />
     </div>
   );
 }
