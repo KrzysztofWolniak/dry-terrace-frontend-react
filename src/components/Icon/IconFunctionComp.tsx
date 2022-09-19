52adf9d3b3bdca1dd4a44590dbebbf191a849ad0
KrzysztofWolniak <261288@student.pwr.edu.pl>
2022-09-20 00:01:10 +0200

    Added onHover option that gives options how to display this component

--- src/components/Icon/IconFunctionComp.tsx
+++ src/components/Icon/IconFunctionComp.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { useTransition, animated } from "react-spring";
 import { IconContext } from "react-icons";
-//Css import 
+//Css import
 import "./Icon.css";
 //our props interface, propably should be moved out of our component
 interface IconProps {
@@ -24,6 +24,7 @@ export const IconFunctionComp = (props: IconProps) => {
   // UseState, isVisable is our only state in this component, we use it to change
   //our text visibility
   const [isVisible, setIsVisible] = useState(false);
+
   // useTransition is hook from react-spring, we call it everytime component is mounting or unmounting
   // with this we can create cool looking animation
   const transition = useTransition(isVisible, {
@@ -31,9 +32,12 @@ export const IconFunctionComp = (props: IconProps) => {
     enter: { opacity: 1, width: "100%" },
     leave: { opacity: 0, width: "0%", padding: "0px" },
   });
-  // arrow function used to determinate longest word in our string we display so we can
-  // set our width based on our font, working also with mobile phones
-  // this function returns word length as a number
+
+  /**
+   * The function takes a string as an argument and returns the length of the longest word in the string
+   * @param {string} str - string
+   * @returns The length of the longest word in the string.
+   */
   const findLongestWord = (str: string): number => {
     var longestWord = str
       .split(" ")
@@ -42,13 +46,11 @@ export const IconFunctionComp = (props: IconProps) => {
       }, "");
     return longestWord.length;
   };
-  // const used to store classname for our component 
-  // it's easier to change one const than writing 2 times same classnames
+
+  /* It's a constant that is used to store the class name for the component. */
   const compConstClassName: string = "textConst";
 
   return (
-    // IconContext.Provider is used to set our react-icons icon value e.g size or color
-    // if we are going to change our icons api then we need to use diffrent provider
     <IconContext.Provider
       value={{
         size: props.size,
@@ -56,38 +58,49 @@ export const IconFunctionComp = (props: IconProps) => {
         className: "icon",
       }}
     >
-      {/* Motion.div comes from framer-motion and with this lib we can create beautiful animations in snap of a finger via animate atribute  */}
-      <motion.div
-        style={{ minWidth: props.size }}
-        animate={{
-          width: isVisible
-            ? `calc(${findLongestWord(props.text!)}em + ${props.size}px - ${
-                findLongestWord(props.text!) * 0.33
-              }em)`
-            : "48px",
-        }}
-        className={
-          isVisible
-            ? `${compConstClassName} showed`
-            : `${compConstClassName} hiden`
-        }
-        // we use this event listners to set our text visibility 
-        onMouseEnter={() => setIsVisible(true)}
-        onMouseLeave={() => setIsVisible(false)}
-      >
-        {/* \/ we create our icon from props, still looking for better solution  */}
-        {React.createElement(props.icon)}
-        {transition((style, item) =>
-          item ? (
-            //animate comes from react-spring and it is used by our useTransition hook
-            <animated.a style={style} className="text">
-              {props.text}
-            </animated.a>
-          ) : (
-            ""
-          )
-        )}
-      </motion.div>
+      {props.onHover ? (
+        <motion.div
+          style={{ minWidth: props.size }}
+          animate={{
+            width: isVisible
+              ? `calc(${findLongestWord(props.text!)}em + ${props.size}px - ${
+                  findLongestWord(props.text!) * 0.33
+                }em)`
+              : `${props.size}px`,
+          }}
+          className={
+            isVisible
+              ? `${compConstClassName} showed`
+              : `${compConstClassName} hiden`
+          }
+          onMouseEnter={() => setIsVisible(true)}
+          onMouseLeave={() => setIsVisible(false)}
+        >
+          {React.createElement(props.icon)}
+          {transition((style, item) =>
+            item ? (
+              <animated.a style={style} className="text">
+                {props.text}
+              </animated.a>
+            ) : (
+              ""
+            )
+          )}
+        </motion.div>
+      ) : (
+        <motion.div
+          style={{ minWidth: props.size }}
+          whileTap={{ y: "0%" }}
+          animate={ isVisible? {y:"-20%"}:{}}
+          className={compConstClassName}
+          onMouseEnter={() => setIsVisible(true)}
+          onMouseLeave={() => setIsVisible(false)}
+        >
+          {React.createElement(props.icon)}
+
+          <a className="text">{props.text}</a>
+        </motion.div>
+      )}
     </IconContext.Provider>
   );
 };
