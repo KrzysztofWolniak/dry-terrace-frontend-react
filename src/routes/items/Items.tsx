d8faaa59ef77c8abf0c31e57a24d96e3b38de5a5
KrzysztofWolniak <261288@student.pwr.edu.pl>
2022-09-26 23:34:08 +0200

    added .map function to map over array of objects

--- src/routes/items/Items.tsx
+++ src/routes/items/Items.tsx
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { ItemContainer } from "../../components/ItemContainer/ItemContainer";
 
 export const Items = () => {
+  const [itemsList, setItemsList] = useState([
+    {
+      id: "1",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptas
+  similique eius quia omnis consequatur illo saepe, suscipit nemo
+  repudiandae reprehenderit, perspiciatis illum? Qui ea provident quae
+  asperiores, recusandae aliquam.`,
+      header: "Roleta rzymska",
+      alt: "Roleta",
+      image: "https://random.imagecdn.app/345/140",
+      price: "140 zł/m2",
+    },
+    {
+      id: "22",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptas
+  similique eius quia omnis consequatur illo saepe, suscipit nemo
+  repudiandae reprehenderit, perspiciatis illum? Qui ea provident quae
+  asperiores, recusandae aliquam.`,
+      header: "Zadaszenie tkaninowe",
+      alt: "Roleta",
+      image: "https://random.imagecdn.app/345/140",
+      price: "140 zł/m2",
+    },
+    {
+      id: "33",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptas
+  similique eius quia omnis consequatur illo saepe, suscipit nemo
+  repudiandae reprehenderit, perspiciatis illum? Qui ea provident quae
+  asperiores, recusandae aliquam.`,
+      header: "Osłona silikonowa",
+      alt: "Roleta",
+      image: "https://random.imagecdn.app/345/140",
+      price: "140 zł/m2",
+    }
+  ]);
   return (
     <div className="Items">
-      <ItemContainer id={1} ></ItemContainer>
-      <ItemContainer id={2}></ItemContainer>
-      <ItemContainer id={3}></ItemContainer>
-      <ItemContainer id={4}></ItemContainer>
-      <ItemContainer id={5}></ItemContainer>
-      <ItemContainer id={6}></ItemContainer>
-      <ItemContainer id={7}></ItemContainer>
+      {itemsList.map((item,key) => (
+        <ItemContainer
+          id={item.id}
+          text={item.text}
+          header={item.header}
+          alt={item.alt}
+          image={item.image}
+          price={item.price}
+          key={key}
+        ></ItemContainer>
+      ))}
+      
     </div>
   );
 };
