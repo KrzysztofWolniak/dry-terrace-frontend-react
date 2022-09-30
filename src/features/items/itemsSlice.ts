4548e56e4a00707af9435f3e26ae184d27a5f3a4
KrzysztofWolniak <261288@student.pwr.edu.pl>
2022-09-30 22:59:37 +0200

    State managment(Redux) added to save the complication of later implementation

---
+++ src/features/items/itemsSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice } from '@reduxjs/toolkit'
+import itemData from '../../interfaces/itemData'
+interface itemsState {
+    itemsList: Array<itemData>
+}
+
+const initialState:itemsState={
+    itemsList: [{
+        id: "1",
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptas
+    similique eius quia omnis consequatur illo saepe, suscipit nemo
+    repudiandae reprehenderit, perspiciatis illum? Qui ea provident quae
+    asperiores, recusandae aliquam.`,
+        header: "Roleta rzymska",
+        alt: "Roleta",
+        image: "https://random.imagecdn.app/345/140",
+        price: "140 zł/m2",
+      },
+      {
+        id: "22",
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptas
+    similique eius quia omnis consequatur illo saepe, suscipit nemo
+    repudiandae reprehenderit, perspiciatis illum? Qui ea provident quae
+    asperiores, recusandae aliquam.`,
+        header: "Zadaszenie tkaninowe",
+        alt: "Roleta",
+        image: "https://random.imagecdn.app/345/140",
+        price: "140 zł/m2",
+      },
+      {
+        id: "33",
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptas
+    similique eius quia omnis consequatur illo saepe, suscipit nemo
+    repudiandae reprehenderit, perspiciatis illum? Qui ea provident quae
+    asperiores, recusandae aliquam.`,
+        header: "Osłona silikonowa",
+        alt: "Roleta",
+        image: "https://random.imagecdn.app/345/140",
+        price: "140 zł/m2",
+      }]
+}
+
+export const itemsSlice = createSlice({
+  name: 'items',
+  initialState,
+  reducers: {
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const {  } = itemsSlice.actions
+
+export default itemsSlice.reducer
