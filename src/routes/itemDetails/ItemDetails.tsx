156bcd007e168295129c67da20331807b354cae9
KrzysztofWolniak <261288@student.pwr.edu.pl>
2022-10-02 00:45:44 +0200

    created basic state of item Details component

--- src/routes/itemDetails/ItemDetails.tsx
+++ src/routes/itemDetails/ItemDetails.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React from "react";
+import { DiAndroid } from "react-icons/di";
+import { Color } from "../../components/Color/Color";
+import { IconFunctionComp } from "../../components/Icon/IconFunctionComp";
+import { useAppSelector } from "../../hooks/reduxHooks";
+import color from "../../interfaces/color";
+import "./ItemDetails.css";
 
-export const ItemDetails = () => {
+export const ItemDetails = (props: any) => {
+  const colorsList:Array<color> = useAppSelector((state) => state.items.availableColors);
   return (
-    <div>ItemDetails</div>
-  )
-}
+    <div className="container-lg text-left">
+      <div className="row align-items-start  ">
+        <div className="col text-center ">
+          <img src="https://random.imagecdn.app/500/500" alt="test" />
+        </div>
+        <div className="col test">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptas
+            similique eius quia omnis consequatur illo saepe, suscipit nemo
+            repudiandae reprehenderit, perspiciatis illum? Qui ea provident quae
+            asperiores, recusandae aliquam.
+          </p>
+          
+          <div className="text-center">
+            <button className="px-sm-3">
+              <IconFunctionComp
+                size="48"
+                color="red"
+                text="Dodaj do koszyka "
+                icon={DiAndroid}
+                onHover
+              />
+            </button>
+          </div>
+          <p>Dostępne kolory</p>
+          <div className=" d-flex  flex-wrap ">
+            {colorsList.map((item,key)=>(
+              <Color key={key} color={item} />
+            )
+
+            )}
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
